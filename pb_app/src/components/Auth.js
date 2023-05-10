11bf656c9585b48e01a22e2c66c14142b3b1ab76
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2023-05-10 22:19:47 +0800

    Auth.js was refactored to reflect the changes in UseVerified.js

--- pb_app/src/components/Auth.js
+++ pb_app/src/components/Auth.js
@@ -1,12 +1,12 @@
 import UseLogin from "hooks/UseLogin";
 import UseLogout from "hooks/UseLogout";
-import UseVerified from "hooks/UseVerified";
+import UseVerified, { requestVerification } from "hooks/UseVerified";
 import PB from "lib/pocketbase";
 import { useForm } from "react-hook-form";
 
 function Auth(){
     const logout = UseLogout();
-    const { is_verified, requestVerification } = UseVerified();
+    const { data: is_verified } = UseVerified();
     const { mutate, isLoading, isError } = UseLogin();
     const { register, handleSubmit, reset } = useForm();
     const is_logged_in = PB.authStore.isValid;
@@ -30,7 +30,7 @@ function Auth(){
             <>
                 {/* If a user has logged in, display the email address */}
                 <h1>Logged In: { is_logged_in && PB.authStore.model.email }</h1>
-                <p>Verified: { is_verified.toString() }</p>
+                <p>Verified: { is_verified?.toString() }</p>
                 { !is_verified && <button onClick={requestVerification}>Send Verification</button> }
                 <button onClick={ logout }>Log Out</button>
             </>
